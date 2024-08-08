277efef1432ad2fe08e05dc337fea30b7b6c9158
ryeong99 <lovolvo1015@gamil.com>
2024-08-08 17:41:09 +0900

    sasd

---
+++ todolist_back/src/main/java/com/study/todolist/config/webMvConfig.java
@@ -0,0 +1,18 @@
+package com.study.todolist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webMvConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
---
+++ todolist_back/src/main/java/com/study/todolist/service/TodoService.java
@@ -0,0 +1,17 @@
+package com.study.todolist.service;
+
+import com.study.todolist.dto.response.todo.ReqAddTodoDto;
+import com.study.todolist.repository.TodoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoMapper todoMapper;
+
+    public int addTodo(ReqAddTodoDto dto) {
+        return todoMapper.save(dto.toEntity());
+    }
+}
